072ee1c6a392599bed3d89fe52532d7d232ecd3a
Rick <hoa98hoa@gmail.com>
2022-06-20 18:57:54 -0300

    Crear useState for input email and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInput = ({ target: { name, value } }) => {
+    switch (name) {
+    case 'email': {
+      setEmail(value);
+      break;
+    } case 'password': {
+      setPassword(value);
+      break;
+    } default: {
+      break;
+    }
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('test');
@@ -9,9 +26,28 @@ function Login() {
   return (
     <form onSubmit={ handleSubmit }>
       <h2>Login</h2>
-      <input type="text" placeholder="Email" data-testid="email-input" />
-      <input type="text" placeholder="Password" data-testid="password-input" />
-      <button type="submit" data-testid="login-submit-btn">Enviar</button>
+      <input
+        data-testid="email-input"
+        name="email"
+        placeholder="Email"
+        type="text"
+        onChange={ handleInput }
+        value={ email }
+      />
+      <input
+        data-testid="password-input"
+        name="password"
+        placeholder="Password"
+        type="text"
+        onChange={ handleInput }
+        value={ password }
+      />
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+      >
+        Enviar
+      </button>
     </form>
   );
 }
