100f53eb3da4cbec1e7ee58ca90ad31fdba94a17
Rick <hoa98hoa@gmail.com>
2022-06-20 19:53:11 -0300

    Creat test to check bnt is disabled

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -7,7 +7,7 @@ const EMAIL_INPUT_TEST_ID = 'email-input';
 const PASSWORD_INPUT_TEST_ID = 'password-input';
 const LOGIN_SUBMIT_BNT_TEST_ID = 'login-submit-btn';
 const VALID_EMAIL = 'test@test.com';
-const VALID_PASSWORD = '123456';
+const VALID_PASSWORD = 'test12';
 
 describe('Testing page Login', () => {
   it('Testing componentes in screen, Requirement 2', () => {
@@ -18,7 +18,7 @@ describe('Testing page Login', () => {
     expect(screen.getByTestId(LOGIN_SUBMIT_BNT_TEST_ID)).toBeInTheDocument();
   });
 
-  it('Testing write in camp input', () => {
+  it('Testing writing in input field', () => {
     render(<Login />);
 
     const inputEmailEL = screen.getByTestId(EMAIL_INPUT_TEST_ID);
@@ -29,5 +29,20 @@ describe('Testing page Login', () => {
 
     expect(inputEmailEL).toHaveValue(VALID_EMAIL);
     expect(inputPassword).toHaveValue(VALID_PASSWORD);
+  });
+
+  it('Testing if form inputs are valid', () => {
+    render(<Login />);
+
+    const inputEmailEL = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const bntSubmit = screen.getByTestId(LOGIN_SUBMIT_BNT_TEST_ID);
+
+    expect(bntSubmit).toBeDisabled();
+
+    useEvent.type(inputEmailEL, VALID_EMAIL);
+    useEvent.type(inputPassword, VALID_PASSWORD);
+
+    expect(bntSubmit).not.toBeDisabled();
   });
 });
