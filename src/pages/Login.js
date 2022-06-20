33f5edd9aa944e4427c3a01c9cf28e9fb295de19
Rick <hoa98hoa@gmail.com>
2022-06-20 18:35:10 -0300

    Creat componentes from login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,19 @@
+import React from 'react';
+
 function Login() {
-  return null;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('test');
+  };
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <h2>Login</h2>
+      <input type="text" placeholder="Email" data-testid="email-input" />
+      <input type="text" placeholder="Password" data-testid="password-input" />
+      <button type="submit" data-testid="login-submit-btn">Enviar</button>
+    </form>
+  );
 }
 
 export default Login;
