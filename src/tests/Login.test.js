090144232893beaba80e0ad9f2059487f883fab2
Rick <hoa98hoa@gmail.com>
2022-06-20 18:37:09 -0300

    Creat test for input req 3, 4

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -6,6 +6,8 @@ import Login from '../pages/Login';
 const EMAIL_INPUT_TEST_ID = 'email-input';
 const PASSWORD_INPUT_TEST_ID = 'password-input';
 const LOGIN_SUBMIT_BNT_TEST_ID = 'login-submit-btn';
+const VALID_EMAIL = 'test@test.com';
+const VALID_PASSWORD = '123456';
 
 describe('Testing page Login', () => {
   it('Testing componentes in screen, Requirement 2', () => {
@@ -14,5 +16,18 @@ describe('Testing page Login', () => {
     expect(screen.getByTestId(EMAIL_INPUT_TEST_ID)).toBeInTheDocument();
     expect(screen.getByTestId(PASSWORD_INPUT_TEST_ID)).toBeInTheDocument();
     expect(screen.getByTestId(LOGIN_SUBMIT_BNT_TEST_ID)).toBeInTheDocument();
+  });
+
+  it('Testing write in camp input', () => {
+    render(<Login />);
+
+    const inputEmailEL = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+
+    useEvent.type(inputEmailEL, VALID_EMAIL);
+    useEvent.type(inputPassword, VALID_PASSWORD);
+
+    expect(inputEmailEL).toHaveValue(VALID_EMAIL);
+    expect(inputPassword).toHaveValue(VALID_PASSWORD);
   });
 });
