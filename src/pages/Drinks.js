d6628bda0ad442ed2f06e08a232e031de309be1b
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-06-21 15:04:58 -0300

    fix: fix lint and headerSearch component

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import HeaderSearch from '../components/HeaderSearch';
 import Header from '../components/Header';
 
 function Drinks() {
   return (
     <div>
       <Header pageName="Drinks" />
-      <HeaderSearch />
     </div>
   );
 }
