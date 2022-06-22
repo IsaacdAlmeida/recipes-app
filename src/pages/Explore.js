8c72fdcf08d0cfb676b7a2bbd248885592cf03b7
Vitor <vieira.vitor42@gmail.com>
2022-06-22 16:29:43 -0300

    feat: create buttons and route

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Explore() {
   return (
-    <Header pageName="Explore" isEnable={ false } />
+    <div>
+      <Header pageName="Explore" isEnable={ false } />
+      <Link to="/explore/foods">
+        <button
+          data-testid="explore-foods"
+          type="button"
+        >
+          Explore Foods
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explore Drinks
+        </button>
+      </Link>
+    </div>
   );
 }
 
