90591854da623ce0ec187cb24ba0d2d28a32a15f
Nina Ferrari <ninapferrari@gmail.com>
2022-06-22 10:53:46 -0300

    requisito 82 concluído

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,6 +5,12 @@ function Profile() {
   return (
     <div>
       <Header pageName="Profile" isEnable={ false } />
+      <section>
+        <p data-testid="profile-email">email@test.com</p>
+        <button data-testid="profile-done-btn" type="button"> Done Recipes</button>
+        <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn" type="button">Logout</button>
+      </section>
     </div>
   );
 }
