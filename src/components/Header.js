6350bd9fdcdc56d23cbb492d5ac8b77278a8300a
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-06-29 15:31:44 -0300

    fix: render the searchbar on new div

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,48 +13,52 @@ function Header({ pageName, isEnable }) {
   const [showSearchBar, setShowSearchBar] = useState(false);
   const { handleChange, search } = useContext(MainContext);
   return (
-    <div className="header-container">
-      <div>
-        <Link to="/profile">
-          <input // https://www.w3schools.com/tags/att_input_src.asp#:~:text=The%20src%20attribute%20specifies%20the,type%3D%22image%22%3E%20.
-            type="image"
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt="profile button"
-          />
-        </Link>
-      </div>
-      <div>
-        <h3 data-testid="page-title">{ pageName }</h3>
-      </div>
-      <div>
-        { isEnable && (
-          <div>
+    <div>
+      <div className="header-container">
+        <div>
+          <Link to="/profile">
+            <input // https://www.w3schools.com/tags/att_input_src.asp#:~:text=The%20src%20attribute%20specifies%20the,type%3D%22image%22%3E%20.
+              type="image"
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="profile button"
+            />
+          </Link>
+        </div>
+        <div>
+          <h3 data-testid="page-title">{ pageName }</h3>
+        </div>
+        <div>
+          { isEnable && (
             <div>
-              <input
-                type="image"
-                data-testid="search-top-btn"
-                src={ searchIcon }
-                alt="search button"
-                onClick={ () => setShowSearchBar(!showSearchBar) }
-              />
-              { showSearchBar
-                && (
-                  <div className="xablau">
-                    <input
-                      data-testid="search-input"
-                      type="text"
-                      value={ search }
-                      onChange={ handleChange }
-                    />
-                    <HeaderSearch />
-                  </div>
-                )}
+              <div>
+                <input
+                  type="image"
+                  data-testid="search-top-btn"
+                  src={ searchIcon }
+                  alt="search button"
+                  onClick={ () => setShowSearchBar(!showSearchBar) }
+                />
+              </div>
             </div>
-          </div>
-        ) }
-      </div>
+          ) }
+        </div>
 
+      </div>
+      <div className="inpunt-container">
+        { showSearchBar
+                  && (
+                    <div className="xablau">
+                      <input
+                        data-testid="search-input"
+                        type="text"
+                        value={ search }
+                        onChange={ handleChange }
+                      />
+                      <HeaderSearch />
+                    </div>
+                  )}
+      </div>
     </div>
   );
 }
